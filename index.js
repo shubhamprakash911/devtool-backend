45d0ec449b0a0b989c5a2bc19aaec3a6aa0d2a45
Shubham Prakash <shubhamprakash9112000@gmail.com>
2024-07-12 15:32:53 +0530

    added code to fixed deployement issue

--- index.js
+++ index.js
@@ -16,45 +16,52 @@ app.get("/devtool", async (req, res) => {
     return res.status(400).send("Please provide a URL");
   }
 
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-
-  const networkData = {
-    html: "",
-    css: [],
-    js: [],
-    xhr: [],
-    images: [],
-    docs: [],
-  };
-
-  page.on("request", (request) => {
-    const type = request.resourceType();
-    const requestUrl = request.url();
-
-    if (type === "stylesheet") {
-      networkData.css.push(requestUrl);
-    } else if (type === "script") {
-      networkData.js.push(requestUrl);
-    } else if (type === "xhr") {
-      networkData.xhr.push(requestUrl);
-    } else if (type === "image") {
-      networkData.images.push(requestUrl);
-    } else if (type === "document") {
-      networkData.docs.push(requestUrl);
-    }
-  });
-
+  let browser;
   try {
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: puppeteer.executablePath(), // Ensure Puppeteer uses the installed Chromium
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+
+    const networkData = {
+      html: "",
+      css: [],
+      js: [],
+      xhr: [],
+      images: [],
+      docs: [],
+    };
+
+    page.on("request", (request) => {
+      const type = request.resourceType();
+      const requestUrl = request.url();
+
+      if (type === "stylesheet") {
+        networkData.css.push(requestUrl);
+      } else if (type === "script") {
+        networkData.js.push(requestUrl);
+      } else if (type === "xhr") {
+        networkData.xhr.push(requestUrl);
+      } else if (type === "image") {
+        networkData.images.push(requestUrl);
+      } else if (type === "document") {
+        networkData.docs.push(requestUrl);
+      }
+    });
+
     await page.goto(url, { waitUntil: "networkidle2" });
     networkData.html = await page.content();
     await browser.close();
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.json(networkData);
   } catch (error) {
-    await browser.close();
+    if (browser) {
+      await browser.close();
+    }
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(500).send("Error while scraping the page");
   }
 });
